602ff8c130629fd6be1affef14cb61450b0deb3a
Srdjan Grubor <sgnn7@sgnn7.org>
2020-01-28 13:03:51 -0600

    Extracted out time formatting string from issues module
    We reuse this string so it needs to be a constant. This change does
    exactly that.

--- Issue.py
+++ Issue.py
@@ -5,7 +5,9 @@ import json
 import requests
 
 class Issue:
-    def __init__(self,raw_json, USER, AUTH_TOKEN):
+    TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+
+    def __init__(self, raw_json, USER, AUTH_TOKEN):
         self.raw_json = raw_json
         self.USER = USER
         self.AUTH_TOKEN = AUTH_TOKEN
@@ -14,25 +16,25 @@ class Issue:
         return self.raw_json['user']['login']
 #returns datetime object of the time
     def get_creation_time(self):
-        return datetime.datetime.strptime(self.raw_json['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+        return datetime.datetime.strptime(self.raw_json['created_at'], Issue.TIME_FORMAT)
 #
     def get_updated_time(self):
         if self.raw_json['updated_at'] == None:
             return None
         else:
-            return datetime.datetime.strptime(self.raw_json['updated_at'], "%Y-%m-%dT%H:%M:%SZ")
+            return datetime.datetime.strptime(self.raw_json['updated_at'], Issue.TIME_FORMAT)
 #
     def get_closed_time(self):
         if self.raw_json['closed_at'] == None:
             return None
         else:
-            return datetime.datetime.strptime(self.raw_json['closed_at'], "%Y-%m-%dT%H:%M:%SZ")
-    
+            return datetime.datetime.strptime(self.raw_json['closed_at'], Issue.TIME_FORMAT)
+
     def get_state(self):
         return self.raw_json['state']
 
     def within_six_months(self):
-        curr_date = datetime.datetime.strptime(self.raw_json['created_at'], "%Y-%m-%dT%H:%M:%SZ").date()
+        curr_date = datetime.datetime.strptime(self.raw_json['created_at'], Issue.TIME_FORMAT).date()
         today = datetime.datetime.today().date()
         six_months = (datetime.datetime.today() - datetime.timedelta(6*365/12)).date()
 
