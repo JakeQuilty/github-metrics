54f962af08c93ef5872f03da82fff321ab6ad560
Srdjan Grubor <sgnn7@sgnn7.org>
2020-01-28 13:08:22 -0600

    Simplified logic and corrected some minor errors in Issues module
    Fixes applied:
    - Simplification of `if` statements
    - Simplification of early exit flows
    - Fixes to a few capitalization issues
    - Fixes to some PEP8 tyling issues
    - Fixed a few variable naming issues

--- Issue.py
+++ Issue.py
@@ -7,41 +7,41 @@ import requests
 class Issue:
     TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
-    def __init__(self, raw_json, USER, AUTH_TOKEN):
+    def __init__(self, raw_json, user, auth_token):
         self.raw_json = raw_json
-        self.USER = USER
-        self.AUTH_TOKEN = AUTH_TOKEN
+        self.USER = user
+        self.AUTH_TOKEN = auth_token
 
     def get_author(self):
         return self.raw_json['user']['login']
-#returns datetime object of the time
+
+    #returns datetime object of the time
     def get_creation_time(self):
         return datetime.datetime.strptime(self.raw_json['created_at'], Issue.TIME_FORMAT)
-#
+
     def get_updated_time(self):
-        if self.raw_json['updated_at'] == None:
+        if self.raw_json['updated_at'] is None:
             return None
-        else:
-            return datetime.datetime.strptime(self.raw_json['updated_at'], Issue.TIME_FORMAT)
-#
+
+        return datetime.datetime.strptime(self.raw_json['updated_at'], Issue.TIME_FORMAT)
+
     def get_closed_time(self):
-        if self.raw_json['closed_at'] == None:
+        if self.raw_json['closed_at'] is None:
             return None
-        else:
-            return datetime.datetime.strptime(self.raw_json['closed_at'], Issue.TIME_FORMAT)
+
+        return datetime.datetime.strptime(self.raw_json['closed_at'], Issue.TIME_FORMAT)
 
     def get_state(self):
         return self.raw_json['state']
 
     def within_six_months(self):
-        curr_date = datetime.datetime.strptime(self.raw_json['created_at'], Issue.TIME_FORMAT).date()
-        today = datetime.datetime.today().date()
+        issue_date = datetime.datetime.strptime(self.raw_json['created_at'], Issue.TIME_FORMAT).date()
+        current_date = datetime.datetime.today().date()
+
+        # TODO: This can be done simpler
         six_months = (datetime.datetime.today() - datetime.timedelta(6*365/12)).date()
 
-        if six_months <= curr_date <= today:
-            return True
-        else:
-            return False
+        return six_months <= issue_date <= current_date
 
     def get_url(self):
         return self.raw_json['url']
