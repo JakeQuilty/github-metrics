63328009aadcc61341ceb9444eb3519b17ab69ea
Srdjan Grubor <sgnn7@sgnn7.org>
2020-01-28 14:16:22 -0600

    Fixed spacing in Repo module
    Lots of comments/code was inconsistent so it's now a bit more standard
    and following PEP8.

--- Repo.py
+++ Repo.py
@@ -11,10 +11,9 @@ from Issue import Issue
 class Repo:
     COMMENTS_URL = "https://api.github.com/repos/{org_name}/{issue_name}/issues/comments?state=all&per_page=100&page=1"
 
-
-    def __init__(self,org_name, issue_name, USER, AUTH_TOKEN, org_members):
-        self.USER = USER
-        self.AUTH_TOKEN = AUTH_TOKEN
+    def __init__(self,org_name, issue_name, user, auth_token, org_members):
+        self.USER = user
+        self.AUTH_TOKEN = auth_token
         self.org_name = org_name
         self.org_members = org_members
 
@@ -22,6 +21,7 @@ class Repo:
         comments_response = requests.get(Repo.COMMENTS_URL.format(org_name=org_name, issue_name=issue_name), auth=(self.USER, self.AUTH_TOKEN))
 
         self.raw_comments = json.loads(comments_response.text)
+
         while 'next' in comments_response.links.keys():
             comments_response = requests.get(comments_response.links['next']['url'], auth=(self.USER, self.AUTH_TOKEN))
             self.raw_comments.extend(json.loads(comments_response.text))
@@ -29,7 +29,8 @@ class Repo:
         self.curr_comment_index = 0
         self.raw_comments.reverse()
 
-        #Get lists of pull requests and issues
+        # Get lists of pull requests and issues
+        # TODO: Use the issue URL from Issue module
         issue_url = "https://api.github.com/repos/"+ org_name + "/" + issue_name + "/issues?state=all&per_page=100&page=1"
         issue_response = requests.get(issue_url, auth=(self.USER, self.AUTH_TOKEN))
         raw_issues = json.loads(issue_response.text)
@@ -44,7 +45,7 @@ class Repo:
         self.six_months = [time.localtime(time.mktime((time.localtime().tm_year, time.localtime().tm_mon - n, 1, 0, 0, 0, 0, 0, 0)))[:2] for n in range(6)]
         self.issue_name = issue_name
 
-#pr
+        # PR
         self.pr_open_states = 0
         self.pr_closed_states = 0
         self.pr_unique_contr = []
@@ -57,7 +58,8 @@ class Repo:
         self.pr_closed_times = []
         self.pr_first_response_times = []
         self.pr_six_months_count = [0] * 6
-#issue
+
+        # Issue
         self.issue_open_states = 0
         self.issue_closed_states = 0
         self.issue_unique_contr = []
@@ -73,71 +75,84 @@ class Repo:
 
         self.get_repo_data(self.pr_list, self.issue_list)
 
-    #sort issue and pr lists
+    # Sort issue and PR lists
     def sort_lists(self, raw_list):
         for issue in raw_list:
             try:
-                flag = issue['pull_request']    ##this will cause an error if the pull_request tag isn't there -> means it's an issue
+                # This will cause an error if the pull_request tag isn't there -> means it's an issue
+                flag = issue['pull_request']
                 self.pr_list.append(Issue(issue, self.USER, self.AUTH_TOKEN))
             except KeyError:
                 self.issue_list.append(Issue(issue, self.USER, self.AUTH_TOKEN))
 
-    #Gets all needed data for repo from each pr/issue. Adds them to their lists
+    # Gets all needed data for repo from each pr/issue. Adds them to their lists
     def get_repo_data(self, prlist, issue_list):
         print("Getting data from " + self.issue_name + "...")
         for pr in prlist:
             self.pr_created_times.append(pr.get_creation_time())
             self.pr_closed_times.append(pr.get_closed_time())
             self.pr_first_response_times.append(self.first_comment(pr.get_url()))
-            if pr.get_state() == "closed":  #get state
-                self.pr_closed_states+=1
+
+            # Get state
+            if pr.get_state() == "closed":
+                self.pr_closed_states += 1
             else:
-                self.pr_open_states+=1
+                self.pr_open_states += 1
 
             author = pr.get_author()
-            unique = self.contributors(author, self.pr_unique_contr, self.pr_unique_contr_count )  #add author to total count
+
+            # Add author to total count
+            unique = self.contributors(author, self.pr_unique_contr, self.pr_unique_contr_count)
             self.pr_unique_contr = unique[0]
             self.pr_unique_contr_count = unique[1]
             if self.author_in_org(author):
-                unique = self.contributors(author, self.pr_in_org_contr, self.pr_in_org_contr_count )  #add author to in org count
+                # Add author to in org count
+                unique = self.contributors(author, self.pr_in_org_contr, self.pr_in_org_contr_count)
                 self.pr_in_org_contr = unique[0]
                 self.pr_in_org_contr_count = unique[1]
             else:
-                unique = self.contributors(author, self.pr_out_org_contr, self.pr_out_org_contr_count )  #add author to out of org count
+                # Add author to out of org count
+                unique = self.contributors(author, self.pr_out_org_contr, self.pr_out_org_contr_count)
                 self.pr_out_org_contr = unique[0]
                 self.pr_out_org_contr_count = unique[1]
 
-            #do this dynamically from repo creation date instead of 6 months?
+            # Do this dynamically from repo creation date instead of 6 months?
             if pr.within_six_months():
                 for date in self.six_months:
                     if pr.get_creation_time().year == date[0] and pr.get_creation_time().month == date[1]:
                         self.pr_six_months_count[self.six_months.index(date)] += 1
                         break
-            
+
         #for issue in issue_list
         for issue in issue_list:
             self.issue_created_times.append(issue.get_creation_time())
             self.issue_closed_times.append(issue.get_closed_time())
             self.issue_first_response_times.append(self.first_comment(issue.get_url()))
-            if issue.get_state() == "closed":  #get state
-                self.issue_closed_states+=1
+
+            # Get state
+            if issue.get_state() == "closed":
+                self.issue_closed_states += 1
             else:
-                self.issue_open_states+=1
+                self.issue_open_states += 1
 
             author = issue.get_author()
-            unique = self.contributors(author, self.issue_unique_contr, self.issue_unique_contr_count )  #add author to total count
+
+            # Add author to total count
+            unique = self.contributors(author, self.issue_unique_contr, self.issue_unique_contr_count)
             self.issue_unique_contr = unique[0]
             self.issue_unique_contr_count = unique[1]
             if self.author_in_org(author):
-                unique = self.contributors(author, self.issue_in_org_contr, self.issue_in_org_contr_count )  #add author to in org count
+                # Add author to in-org total count
+                unique = self.contributors(author, self.issue_in_org_contr, self.issue_in_org_contr_count)
                 self.issue_in_org_contr = unique[0]
                 self.issue_in_org_contr_count = unique[1]
             else:
-                unique = self.contributors(author, self.issue_out_org_contr, self.issue_out_org_contr_count )  #add author to out of org count
+                # Add author to out-of-org total count
+                unique = self.contributors(author, self.issue_out_org_contr, self.issue_out_org_contr_count)
                 self.issue_out_org_contr = unique[0]
                 self.issue_out_org_contr_count = unique[1]
 
-            #do this dynamically from repo creation date instead of 6 months?
+            # Do this dynamically from repo creation date instead of 6 months?
             if issue.within_six_months():
                 for date in self.six_months:
                     if issue.get_creation_time().year == date[0] and issue.get_creation_time().month == date[1]:
@@ -150,28 +165,28 @@ class Repo:
 #traversing whole list every time
         for index in range(len(self.raw_comments)):
             curr_comment_url = self.raw_comments[index]['issue_url']
-            if  curr_comment_url == issue_url: 
+            if  curr_comment_url == issue_url:
                 try:
                     return datetime.datetime.strptime(self.raw_comments[index]["created_at"], "%Y-%m-%dT%H:%M:%SZ")
                 except KeyError:
                     return None
         return None
 
-#returns true or false if the author is a member or the current organization
+    # Returns true or false if the author is a member or the current organization
     def author_in_org(self, author):
         return author in self.org_members
 
-#Updates parallel lists of contributors and their # of contributions
-    def contributors(self,curr_contr,contr_list, contr_count):
+    # Updates parallel lists of contributors and their # of contributions
+    def contributors(self, curr_contr, contr_list, contr_count):
         try:
-         contr_count[contr_list.index(curr_contr)]+=1
+            contr_count[contr_list.index(curr_contr)] += 1
         except ValueError:
-         contr_list.append(curr_contr)
-         contr_count.append(1)
+            contr_list.append(curr_contr)
+            contr_count.append(1)
 
         return [contr_list, contr_count]
 
-#returns FIRST author in list with most contributions
+    # Returns FIRST author in list with most contributions
     def top_contributor(self, contr_list, contr_count):
         if len(contr_list) == 0:
             return None
@@ -184,20 +199,21 @@ class Repo:
                 index = x
         return contr_list[index]
 
-#find difference in time
+    # Find difference in time
     def time_difference(self, first, second):
         dif = second - first
 
         return dif.seconds
 
-#returns avg time in hours of two lists of times
+    # Returns avg time in hours of two lists of times
     def avg_time(self, created_times, stopped_times):
         count = None
         num_of_times = 0
         for x in range(len(created_times)):
             if len(stopped_times) > 0:
-                if stopped_times[x] is None:    #does not count open prs
-                        continue
+                # XXX: Does not count open PRs
+                if stopped_times[x] is None:
+                    continue
             if count is None:
                 count = self.time_difference(created_times[x], stopped_times[x])
                 num_of_times += 1
@@ -206,9 +222,11 @@ class Repo:
                 num_of_times += 1
         if count == None:
             return None
-        return (count / num_of_times)/3600 ##seconds to hours
 
-#format last 6 months of prs
+        # Seconds -> hours
+        return (count / num_of_times)/3600
+
+    # Format last 6 months of prs
     def format_last_six_months_by_month(self, count):
         to_return = {
             'total': sum(count)
@@ -220,10 +238,10 @@ class Repo:
                 'month': date[1],
                 'count': count[x]
             }
-            x+=1
+            x += 1
         return to_return
 
-#format contributor data
+    # Format contributor data
     def format_contributor_list(self, contr_list, contr_count):
         to_return = {
             'topContributor': self.top_contributor(contr_list, contr_count),
@@ -236,7 +254,7 @@ class Repo:
             }
         return to_return
 
-#Runs all the methods that fio the data and puts it into a dict
+    # Runs all the methods that fio the data and puts it into a dict
     def export(self):
 
         to_export = {
@@ -247,7 +265,7 @@ class Repo:
             'lastSixMonthPR': self.format_last_six_months_by_month(self.pr_six_months_count),
             'avgTimeToFirstResponsePR': self.avg_time(self.pr_created_times, self.pr_closed_times),
             'avgTimeToResolutionPR': self.avg_time(self.pr_created_times, self.pr_first_response_times),
-            'allPRAuthors': self.format_contributor_list(self.pr_unique_contr,self.pr_unique_contr_count),
+            'allPRAuthors': self.format_contributor_list(self.pr_unique_contr, self.pr_unique_contr_count),
             'inOrgPRAuthors': self.format_contributor_list(self.pr_in_org_contr, self.pr_in_org_contr_count),
             'outOrgContributorsPR': self.format_contributor_list(self.pr_out_org_contr, self.pr_out_org_contr_count),
             'totalIssue': len(self.issue_list),
@@ -256,9 +274,10 @@ class Repo:
             'lastSixMonthIssue': self.format_last_six_months_by_month(self.issue_six_months_count),
             'avgTimeToFirstResponseIssue': self.avg_time(self.issue_created_times, self.issue_closed_times),
             'avgTimeToResolutionIssue': self.avg_time(self.issue_created_times, self.issue_first_response_times),
-            'allIssueAuthors': self.format_contributor_list(self.issue_unique_contr,self.issue_unique_contr_count),
+            'allIssueAuthors': self.format_contributor_list(self.issue_unique_contr, self.issue_unique_contr_count),
             'inOrgIssueAuthors': self.format_contributor_list(self.issue_in_org_contr, self.issue_in_org_contr_count),
             'outOrgIssueAuthors': self.format_contributor_list(self.issue_out_org_contr, self.issue_out_org_contr_count)
 
         }
+
         return to_export
